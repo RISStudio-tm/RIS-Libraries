21f7df7d2f841822aab978a97dfdbd29961c9531
Daerius-rs <dima.armawir@yandex.ru>
2020-11-01 12:14:55 +0300

    RIS.Logging: fix start and stop log line view && fix ActivatedLog = false after create log

--- RIS.Logging/Log.cs
+++ RIS.Logging/Log.cs
@@ -23,18 +23,18 @@ namespace RIS.Logging
         public string FileFullName { get; private set; }
         public Encoding FileEncoding { get; private set; }
 
-        public Log(string path)
-            : this(path, Encoding.UTF8)
+        public Log(string directoryPath)
+            : this(directoryPath, Encoding.UTF8)
         {
 
         }
-        public Log(string path, Encoding encoding)
+        public Log(string directoryPath, Encoding encoding)
         {
             ActivatedLog = false;
             LogFile = null;
             FileEncoding = encoding;
 
-            Create(path, encoding);
+            Create(directoryPath, encoding);
         }
 
         public void OnInformation(RInformationEventArgs e)
@@ -62,6 +62,94 @@ namespace RIS.Logging
         public void OnError(object sender, RErrorEventArgs e)
         {
             Error?.Invoke(sender, e);
+        }
+
+        private void Create(string directoryPath, Encoding encoding)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                var exception = new DirectoryNotFoundException("Невозможно создать лог-файл, так как указанный каталог не существует");
+                Events.OnError(this, new RErrorEventArgs(exception.Message, exception.StackTrace));
+                OnError(new RErrorEventArgs(exception.Message, exception.StackTrace));
+                throw exception;
+            }
+
+            try
+            {
+                if (!Path.IsPathRooted(directoryPath))
+                    directoryPath = Path.GetFullPath(directoryPath);
+
+                string fileDirectory = Path.Combine(directoryPath, "Logs");
+
+                if (!Directory.Exists(fileDirectory))
+                    Directory.CreateDirectory(fileDirectory);
+
+                DateTime now = DateTime.Now;
+
+                string day = now.Day.ToString().PadLeft(2, '0');
+                string month = now.Month.ToString().PadLeft(2, '0');
+                string year = now.Year.ToString().PadLeft(4, '0');
+                string hours = now.Hour.ToString().PadLeft(2, '0');
+                string minutes = now.Minute.ToString().PadLeft(2, '0');
+                string seconds = now.Second.ToString().PadLeft(2, '0');
+
+                FileName = day + "-" + month + "-" + year + "_" + hours + "-" + minutes + "-" + seconds;
+                FileExtension = ".rlog";
+                FileFullName = FileName + FileExtension;
+                FileDirectory = fileDirectory;
+                FileFullPath = Path.Combine(FileDirectory, FileFullName);
+
+                LogFile = new StreamWriter(FileFullPath, true, encoding);
+                ActivatedLog = true;
+
+                WriteLine("Start", LogSituation.LogAction);
+            }
+            catch (Exception ex)
+            {
+                Events.OnError(this, new RErrorEventArgs(ex.Message, ex.StackTrace));
+                OnError(new RErrorEventArgs(ex.Message, ex.StackTrace));
+                throw;
+            }
+        }
+
+        public void Pause()
+        {
+            ActivatedLog = false;
+
+            WriteLine("Pause", LogSituation.LogAction);
+        }
+
+        public void Resume()
+        {
+            ActivatedLog = true;
+
+            WriteLine("Resume", LogSituation.LogAction);
+        }
+
+        public void Close()
+        {
+            try
+            {
+                WriteLine("Stop", LogSituation.LogAction);
+
+                ActivatedLog = false;
+                LogFile.Close();
+
+                LogFile = null;
+                FileEncoding = null;
+
+                FileDirectory = string.Empty;
+                FileFullPath = string.Empty;
+                FileName = string.Empty;
+                FileExtension = string.Empty;
+                FileFullName = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                Events.OnError(this, new RErrorEventArgs(ex.Message, ex.StackTrace));
+                OnError(new RErrorEventArgs(ex.Message, ex.StackTrace));
+                throw;
+            }
         }
 
         public void WriteLine(string text, LogSituation situation = LogSituation.Unknown)
@@ -96,95 +184,6 @@ namespace RIS.Logging
             string text = string.Empty.PadRight(charsCount, lineChar);
 
             WriteLine(text, LogSituation.LogAction);
-        }
-
-        private void Create(string path, Encoding encoding)
-        {
-            if (!Directory.Exists(path))
-            {
-                var exception = new DirectoryNotFoundException("Невозможно создать лог-файл, так как указанный каталог не существует");
-                Events.OnError(this, new RErrorEventArgs(exception.Message, exception.StackTrace));
-                OnError(new RErrorEventArgs(exception.Message, exception.StackTrace));
-                throw exception;
-            }
-
-            try
-            {
-                if (!Path.IsPathRooted(path))
-                    path = Path.GetFullPath(path);
-
-                string fileDirectory = Path.Combine(path, "Logs");
-
-                if (!Directory.Exists(fileDirectory))
-                {
-                    Directory.CreateDirectory(fileDirectory);
-                }
-
-                DateTime now = DateTime.Now;
-
-                string day = now.Day.ToString().PadLeft(2, '0');
-                string month = now.Month.ToString().PadLeft(2, '0');
-                string year = now.Year.ToString().PadLeft(4, '0');
-                string hours = now.Hour.ToString().PadLeft(2, '0');
-                string minutes = now.Minute.ToString().PadLeft(2, '0');
-                string seconds = now.Second.ToString().PadLeft(2, '0');
-
-                FileName = day + "-" + month + "-" + year + "_" + hours + "-" + minutes + "-" + seconds;
-                FileExtension = ".rlog";
-                FileFullName = FileName + FileExtension;
-                FileDirectory = fileDirectory;
-                FileFullPath = Path.Combine(FileDirectory, FileFullName);
-
-                LogFile = new StreamWriter(FileFullPath, true, encoding);
-
-                WriteLine("Start", LogSituation.LogAction);
-            }
-            catch (Exception ex)
-            {
-                Events.OnError(this, new RErrorEventArgs(ex.Message, ex.StackTrace));
-                OnError(new RErrorEventArgs(ex.Message, ex.StackTrace));
-                throw;
-            }
-        }
-
-        public void Pause()
-        {
-            ActivatedLog = false;
-
-            WriteLine("Pause", LogSituation.LogAction);
-        }
-
-        public void Resume()
-        {
-            ActivatedLog = true;
-
-            WriteLine("Resume", LogSituation.LogAction);
-        }
-
-        public void Close()
-        {
-            try
-            {
-                ActivatedLog = false;
-                LogFile.Close();
-
-                LogFile = null;
-                FileEncoding = null;
-
-                FileDirectory = string.Empty;
-                FileFullPath = string.Empty;
-                FileName = string.Empty;
-                FileExtension = string.Empty;
-                FileFullName = string.Empty;
-
-                WriteLine("Stop", LogSituation.LogAction);
-            }
-            catch (Exception ex)
-            {
-                Events.OnError(this, new RErrorEventArgs(ex.Message, ex.StackTrace));
-                OnError(new RErrorEventArgs(ex.Message, ex.StackTrace));
-                throw;
-            }
         }
     }
 }
