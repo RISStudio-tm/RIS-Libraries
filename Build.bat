a4f53830dea7f2c59de3545eaee23b97df2fce66
Daerius-rs <dima.armawir@yandex.ru>
2021-01-21 22:20:46 +0300

    General: add empty _packages directory (to save the folder structure for building and publishing packages) & update BuildPackages.bat (now gets the path using vswhere.exe) & add Build.bat and BuildDebug.bat (for build in Release and Debug configurations)

--- Build.bat
+++ Build.bat
@@ -1,7 +1,7 @@
 @echo off
 for /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -prerelease -latest -property installationPath`) do (
   if exist "%%i\Common7\Tools\vsdevcmd.bat" (
-    %comspec% /k " "%%i\Common7\Tools\vsdevcmd.bat" & cd "%~dp0" & echo on & dotnet build -c "Release Nuget" "RIS-Libraries.sln" & exit "
+    %comspec% /k " "%%i\Common7\Tools\vsdevcmd.bat" & cd "%~dp0" & echo on & dotnet build -c "Release" "RIS-Libraries.sln" & exit "
     exit /b
   )
 )
