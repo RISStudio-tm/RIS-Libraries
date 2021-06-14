96770d9c95ad5113ee7fc1b22be03a3cfd290ef0
Daerius-rs <dima.armawir@yandex.ru>
2021-06-14 06:17:10 +0300

    RIS.Synchronization: add 'RIS.Synchronization.Context' namespace RIS.Synchronization.Context: add SingleThreadedSynchronizationContext class

---
+++ RIS/Synchronization/Context/SingleThreadedSynchronizationContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RIS.Synchronization.Context
+{
+    public sealed class SingleThreadedSynchronizationContext : SynchronizationContext
+    {
+        private readonly BlockingCollection<(SendOrPostCallback callback, object state)> _queue;
+
+        public SingleThreadedSynchronizationContext()
+        {
+            _queue = new BlockingCollection<(SendOrPostCallback, object)>();
+        }
+
+        public override void Post(SendOrPostCallback callback, object state)
+        {
+            _queue.Add((callback, state));
+        }
+
+        public static void Await(Func<Task> task)
+        {
+            var originalContext = Current;
+
+            try
+            {
+                var context = new SingleThreadedSynchronizationContext();
+
+                SetSynchronizationContext(context);
+
+                var resultTask = task();
+
+                resultTask.ContinueWith(_ =>
+                    context._queue.CompleteAdding());
+
+                while (context._queue.TryTake(
+                    out var work, Timeout.Infinite))
+                {
+                    work.callback(work.state);
+                }
+
+                resultTask.GetAwaiter()
+                    .GetResult();
+            }
+            finally
+            {
+                SetSynchronizationContext(originalContext);
+            }
+        }
+    }
+}
