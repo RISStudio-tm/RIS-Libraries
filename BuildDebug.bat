cb93824f881b8735ae436eeeb638c9ab22036af7
Daerius-rs <dima.armawir@yandex.ru>
2021-07-05 18:44:00 +0300

    General: update nuget CLI to v5.10.0 & call dotnet directly, not via vs console in bat files for build;

--- BuildDebug.bat
+++ BuildDebug.bat
@@ -1,8 +1,2 @@
 @echo off
-for /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -prerelease -latest -property installationPath`) do (
-  if exist "%%i\Common7\Tools\vsdevcmd.bat" (
-    %comspec% /k " "%%i\Common7\Tools\vsdevcmd.bat" & cd "%~dp0" & echo on & dotnet build -c "Debug" "RIS-Libraries.sln" & exit "
-    exit /b
-  )
-)
-exit /b 2
+dotnet build -c "Debug" "RIS-Libraries.sln"
